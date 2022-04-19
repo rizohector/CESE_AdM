65b09066aee3a3f8c4f4a2828a5d34a1c4e3f9ce
rizohector <rizohector@gmail.com>
2022-04-19 11:42:07 -0300

    29vo_commit-AdM

---
+++ Ejercicios/Ej01asm/asm_func.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <stdint.h>
+
+
+void asm_svc (void);
+uint32_t asm_sum (uint32_t firstOperand, uint32_t secondOperand);
+void asm_zeros (uint32_t * vector, uint32_t longitud);
