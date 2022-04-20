e71046f894198d5ed9aff6f81e5ac27e62d874dd
rizohector <rizohector@gmail.com>
2022-04-20 01:53:50 -0300

    31ro_commit-AdM

--- Ejercicios/Ej04asm/asm_func.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-
-void asm_svc (void);
-uint32_t asm_sum (uint32_t firstOperand, uint32_t secondOperand);
-void asm_productoEscalar12bits (uint16_t * vectorIn, uint16_t * vectorOut, uint32_t longitud, uint16_t escalar);
